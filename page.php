8ae33f8f514af43e5f32cdbcd5c6b3469a458fba
Alex Hillman <dangerouslyawesome@gmail.com>
2018-04-14 16:28:45 -0400

    add subpage toggle for announce bar

--- page.php
+++ page.php
@@ -17,6 +17,7 @@ get_header();
 $image = get_field('hero_image');
 $hero_offset = get_field('hero_offset');
 $hero_description = get_field('hero_description');
+$show_announce_bar = get_field('show_announce_bar');
 
 if ($image){
 ?>
@@ -35,12 +36,13 @@ else {
 		<?php echo $hero_description ?>
 	</span>
 	<a href="/tour" class="tour-button">Schedule a Tour</a>
-
 	<?php } ?>
 </section>
+<?php if($show_announce_bar){ ?>
 <section class="upcoming-event">
 	<span class="upcoming-event-content"><?php the_field('upcoming_events', 'option'); ?></span>
 </section>
+<?php } ?>
 <div class="primary content-area">
 	<main id="main" class="site-main">
 		<?php
