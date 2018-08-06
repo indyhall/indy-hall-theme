95c1757582359858cd41ad4b1a2dcd76bed4a1df
Alex Hillman <dangerouslyawesome@gmail.com>
2018-08-06 16:49:37 -0400

    add querystring function to footer

--- footer.php
+++ footer.php
@@ -57,6 +57,18 @@
 	?>
 </nav>
 <div class="off-canvas-screen"></div>
+<script type="text/javascript">
+	function getQueryVariable(variable)
+	{
+	       var query = window.location.search.substring(1);
+	       var vars = query.split("&");
+	       for (var i=0;i<vars.length;i++) {
+	               var pair = vars[i].split("=");
+	               if(pair[0] == variable){return pair[1];}
+	       }
+	       return(false);
+	}
+</script>
 </body>
 </html>
 
